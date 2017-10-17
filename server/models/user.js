e10f6cc5516b850ba825f2cf50ea9a7193e5301e
Philip10023 <philipormorgan@gmail.com>
2017-10-17 15:44:41 -0400

    debugging

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,29 @@ UserSchema.statics.findByToken = function (token) {
   })
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      console.log('something wrong!')
+      return Promise.reject()
+    }
+
+    return new Promise((resolve, reject) => {
+      bcyrpt.compare(password, user.password, (err, res) => {
+        if (res) {
+          consele.log('resolving user')
+          resolve(user)
+        } else {
+          console.log('something went wrong')
+          reject()
+        }
+      })
+    })
+  })
+}
+
 UserSchema.pre('save', function (next) {
   var user = this
 
